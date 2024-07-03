9cf5f91650e1ddd3b7b6487b50b77b4026ad11fb
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-03 20:52:43 +0600

    getter

--- oop-ts/app.ts
+++ oop-ts/app.ts
@@ -2,11 +2,15 @@ class Person {
     private name: string;
     private age: number;
 
-    public constructor(name: string, age: number){
+    public constructor(name: string, age: number) {
         this.name = name;
         this.age = age;
+    }
+
+    public getName(): string {
+        return this.name;
     }
 }
 
 const mugdho = new Person("Mugdho", 23);
-console.log(mugdho)
+console.log(mugdho.getName())
