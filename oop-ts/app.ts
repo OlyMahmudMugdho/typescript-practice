e1a5767f72cb2a428440af834a07d4641fc19cd9
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-03 20:57:04 +0600

    setter

--- oop-ts/app.ts
+++ oop-ts/app.ts
@@ -1,6 +1,7 @@
 class Person {
     private name: string;
     private age: number;
+    private hobby: string;
 
     public constructor(name: string, age: number) {
         this.name = name;
@@ -10,7 +11,17 @@ class Person {
     public getName(): string {
         return this.name;
     }
+
+    public setHobby(hobby: string) {
+        this.hobby = hobby;
+    }
+
+    public getHobby(): string {
+        return this.hobby;
+    }
 }
 
 const mugdho = new Person("Mugdho", 23);
 console.log(mugdho.getName())
+mugdho.setHobby("Learning Arabic")
+console.log(mugdho.getHobby())
