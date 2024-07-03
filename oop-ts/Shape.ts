7aa4a5ca65e8a34d98b3bfc366912357c29bdaf1
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-03 21:19:16 +0600

    Triangle implements Shape

--- oop-ts/Shape.ts
+++ oop-ts/Shape.ts
@@ -1,4 +1,23 @@
-export interface Shape {
+interface Shape {
     edges: number;
     getArea: () => number;
+}
+
+export class Triangle implements Shape {
+    edges: number = 3;
+    height!: number;
+    base!: number;
+
+    getArea(): number {
+        return 0.5 * this.base * this.height;
+    }
+
+    public setHeight(height: number) {
+        return this.height;
+    }
+
+    public setBase(base: number) {
+        return this.base;
+    }
+
 }
